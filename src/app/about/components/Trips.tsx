d283a87a95d5181b1e764855e706903296e77cc0
unknown <brennerpaiva@gmail.com>
2023-07-05 12:18:04 -0300

    create folder of pages and components

---
+++ src/app/about/components/Trips.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { prisma } from "@/app/lib/prisma";
+
+const getTrips = async () => {
+  const trips = await prisma.trip.findMany({});
+
+  return trips;
+};
+
+const Trips = async () => {
+  const data = await getTrips();
+
+  console.log(data);
+  return <div>Trips</div>;
+};
+
+export default Trips;
