776f5d228bccbcdfdf0c96311a55bb85cc24d652
unknown <brennerpaiva@gmail.com>
2023-07-07 19:13:13 -0300

    feat: create input component

--- src/app/components/TripSearch/index.tsx
+++ src/app/components/TripSearch/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Input from "@/components/Input";
 
 const TripSearch = () => {
   return (
@@ -6,6 +7,9 @@ const TripSearch = () => {
       <h2 className="font-semibold text-2xl text-primaryDarker text-center">
         Encontre sua próxima <span className="text-primary">viagem!</span>
       </h2>
+      <div className="flex flex-col">
+        <Input placeholder="Onde você quer ir?" />
+      </div>
     </div>
   );
 };
