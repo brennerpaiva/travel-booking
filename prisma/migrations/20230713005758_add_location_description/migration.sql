f56bd89c372ff76c0cc21d6cb893e624d4f7b8f5
unknown <brennerpaiva@gmail.com>
2023-07-13 11:04:18 -0300

    feat: add location description

---
+++ prisma/migrations/20230713005758_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don''t look even slightly believable.';
