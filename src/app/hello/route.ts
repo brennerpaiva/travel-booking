3240d9a8dcc85866b8e40ced3ab87dc77811cd7b
unknown <brennerpaiva@gmail.com>
2023-07-21 11:29:18 -0300

    feat: add route to get user reservations

--- src/app/hello/route.ts
+++ src/app/hello/route.ts
@@ -1,10 +1,3 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
-import React from "react";
-
-export async function GET() {
-  const trips = await prisma.trip.findMany();
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  return new NextResponse(JSON.stringify(trips), { status: 200 });
+export async function GET(request: Request) {
+  return new Response("Hello, Next.js!");
 }
