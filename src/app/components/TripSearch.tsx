5dd376feeeea6f88fdd7cb070745f90660f6ce7f
unknown <brennerpaiva@gmail.com>
2023-07-21 12:17:45 -0300

    feat: add trip search

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -1,24 +1,84 @@
 "use client";
 
 import React from "react";
+import { useRouter } from "next/navigation";
 import Input from "@/components/Input";
 import DatePicker from "@/components/DatePicker";
 import CurrencyInput from "@/components/CurrencyInput";
 import Button from "@/components/Button";
+import { Controller, useForm } from "react-hook-form";
+
+interface TripSearchForm {
+  text: string;
+  startDate: Date | null;
+  budget: string;
+}
 
 const TripSearch = () => {
+  const router = useRouter();
+
+  const {
+    control,
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm<TripSearchForm>();
+
+  const onSubmit = (data: TripSearchForm) => {
+    router.push(
+      `/trips/search?text=${
+        data.text
+      }&startDate=${data.startDate?.toISOString()}&budget=${data.budget}`
+    );
+  };
+
   return (
     <div className="container mx-auto p-5">
       <h1 className="font-semibold text-2xl text-primaryDarker text-center">
         Encontre sua próxima <span className="text-primary">viagem!</span>
       </h1>
       <div className="flex flex-col gap-4 mt-5">
-        <Input placeholder="Onde Você quer ir?" />
+        <Input
+          placeholder="Onde você quer ir?"
+          error={!!errors.text}
+          errorMessage={errors.text?.message}
+          {...register("text", {
+            required: {
+              value: true,
+              message: "Texto é obrigatório.",
+            },
+          })}
+        />
         <div className="flex gap-4">
-          <DatePicker placeholderText="Data de Ida" onChange={() => {}} />
-          <CurrencyInput placeholder="Orçamento" />
+          <Controller
+            name="startDate"
+            control={control}
+            render={({ field }) => (
+              <DatePicker
+                onChange={field.onChange}
+                selected={field.value}
+                placeholderText="Data Final"
+                className="w-full"
+                minDate={new Date()}
+              />
+            )}
+          />
+
+          <Controller
+            name="budget"
+            control={control}
+            render={({ field }) => (
+              <CurrencyInput
+                allowDecimals={false}
+                placeholder="Orçamento"
+                onValueChange={field.onChange as any}
+                value={field.value}
+                onBlur={field.onBlur}
+              />
+            )}
+          />
         </div>
-        <Button>Pesquisar</Button>
+        <Button onClick={() => handleSubmit(onSubmit)()}>Buscar</Button>
       </div>
     </div>
   );
