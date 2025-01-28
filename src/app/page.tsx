ff9e5ad8a0330d0cd4291fa80bb62da966096a5b
Brenner Paiva <brennerpaiva@gmail.com>
2025-01-28 20:22:31 -0300

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import { prisma } from "@/lib/prisma";
 import QuickSearch from "./components/QuickSearch";
-import RecommendedTrips from "./components/RecomendedTrips";
+import RecomendedTrips from "./components/RecomendedTrips";
 import TripSearch from "./components/TripSearch";
 
-export default async function Home() {
-  const trips = await prisma.trip.findMany();
+export default function Home() {
   return (
     <div>
       <TripSearch />
       <QuickSearch />
-      <RecommendedTrips trips={trips} />
+      <RecomendedTrips />
     </div>
   );
 }
